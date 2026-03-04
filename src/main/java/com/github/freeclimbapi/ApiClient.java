747fb3647256b89939992fb715b82628d705b585
Ajevan M <amahadaya@vailsys.com>
2026-03-04 12:33:02 -0500

    Fix package version for Java SDK and add changelog entry

--- src/main/java/com/github/freeclimbapi/ApiClient.java
+++ src/main/java/com/github/freeclimbapi/ApiClient.java
@@ -121,7 +121,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/6.2.1/java");
+        setUserAgent("OpenAPI-Generator/6.3.1/java");
 
         authentications = new HashMap<String, Authentication>();
     }
